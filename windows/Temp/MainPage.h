1e15e3ea48cf7be6c790e05471b15d78ba3b7f2c
Akshay <akshayms@boston-technology.com>
2023-07-21 17:18:11 +0530

    First Commit.

---
+++ windows/Temp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Temp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Temp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
